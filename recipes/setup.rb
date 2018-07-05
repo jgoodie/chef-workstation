0c8878beb111c01d993b293989b31b1a6da0c519
vagrant <vagrant@localhost.localdomain>
2018-07-05 22:24:34 +0000

    init check in

---
+++ recipes/setup.rb
@@ -0,0 +1,22 @@
+package 'tree' do
+	action [ :install ]
+end
+
+package 'git' do
+	action [ :install ]
+end
+
+package 'ntp' do
+	action [ :install ]
+end
+
+service 'ntpd' do
+	action [ :enable, :start ]
+end
+
+file '/etc/motd' do
+	content 'Property of me'
+	mode '0644'
+	owner 'root'
+	group 'root'
+end
